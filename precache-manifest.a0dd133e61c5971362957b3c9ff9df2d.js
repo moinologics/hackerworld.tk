f81b71b5a94347025c04662039ecfe83697cf167
Moin Ahmad <ahmadmoin723@gmail.com>
2021-07-20 23:41:35 +0530

    adding production build of frontend

---
+++ precache-manifest.a0dd133e61c5971362957b3c9ff9df2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b34f28fc63c56451e1",
+    "url": "/css/app.1d014904.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2fe38f60907651a60049e219316cbde0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdccab027fafc252018a",
+    "url": "/js/about.ff32cdf2.js"
+  },
+  {
+    "revision": "35b34f28fc63c56451e1",
+    "url": "/js/app.7c38af20.js"
+  },
+  {
+    "revision": "35299f14dc931fbda8f7",
+    "url": "/js/chunk-vendors.74544663.js"
+  },
+  {
+    "revision": "6b3ce90e5f19e18664283e9277f759b1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
